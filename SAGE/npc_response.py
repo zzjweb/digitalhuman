6dcfa78b6556e7398a82809e383bd60a67fe19eb
prvmaxzhang <1558914711@qq.com>
2025-05-23 13:22:39 +0800

    Update npc_response.py

--- SAGE/npc_response.py
+++ SAGE/npc_response.py
@@ -11,7 +11,7 @@ import time
 def call_npc(player_history):
 
     # use player_history to build the chat message
-    role_system = '''你是一个智能聊天伙伴，你擅长高情商地和用户聊天，让用户感到舒适、愉快或得到需要的帮助。下面是你和用户的对话上下文：\n''' 
+    role_system = '''你是一个智能聊天伙伴，你擅长高情商地和用户聊天，让用户感到舒适、愉快或得到需要的帮助。''' 
     history = [{"role": "system", "content": role_system}]
     for mes in player_history:
         if mes["role"]=="user":
